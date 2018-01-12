804434129622c4c8625cd852a7ce18d947034753
chchs <chchs@DESKTOP-TERHSA5>
2018-01-12 22:35:00 +0800

    Font support
    Signed-off-by: chchs <chchs@DESKTOP-TERHSA5>

---
+++ src/main/java/com/xxxx/gift/web/controller/FontController.java
@@ -0,0 +1,29 @@
+package com.xxxx.gift.web.controller;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.xxxx.gift.web.service.SfntlyService;
+
+@RestController
+@RequestMapping("/fonts")
+public class FontController {
+	private static final Logger LOGGER = LoggerFactory.getLogger(FontController.class);
+
+	@Autowired
+	SfntlyService sfntlyService;
+	
+	@RequestMapping("/{fontFamily}.ttf")
+	public byte[] getSubFont(@PathVariable String fontFamily, @RequestParam(value="key") String subString) throws IOException {
+		LOGGER.info("get sub font for {},{}",fontFamily,subString);
+		return sfntlyService.getSubFont(fontFamily, subString);
+	}
+	
+}
